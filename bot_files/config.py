e471381a3620d4fa1fa5562b5691a609cbafc9c4
gorpoorko <68359752+gorpoorko@users.noreply.github.com>
2021-06-22 16:13:24 -0300

    Update config.py

--- bot_files/config.py
+++ bot_files/config.py
@@ -22,17 +22,17 @@ logs = os.environ['LOGS']
 sudoers = os.environ['SUDOERS']
 
 LOCAL CONFIG rodar em local host use as linhas abaixo--->
-token =  "1096480409:AAE6sg6eJZtH5Z_TEIzgq10SQtCvGf4KYSc"
-logs = -1001402280935
-sudoers = [522510051,]
+token =  "1096480409:AAE6sqweqwevGf4KYSc"
+logs = -10014022qweqwe
+sudoers = [52qweqwe10051,]
 backups_chat = 522510051  # Put a 0, False or None to disable ou seu id privado ou id privado de um canal ou grupo
 keys = dict(
     here = {'app_id': 'key_id_here', 'app_code': 'key_code_here'},  # https://here.com
-    yandex = 'trnsl.1.1.20190811T184100Z.f3e1e6d6d3507525.7ea9c786af32b18cedeb125ca46cc2d9ee154e09',
-    giphy = '7f6ws7EvslO9BuaAKie9BieyYnD3OkkT',
-    token_dropbox = 's4rxVFP2mcAAAAAAAAC2eXinDL33K0tSIhnR1chqOmrtBPy-Dl6Ll0znnRP3Phm5',
-    token_imgur = "ebfc2558bda96e5",
-    token_bitly = ["a001cef9d44ed8083ed4d952d475e98079e60577", ],  #link?
+    yandex = 'trnsl.1.1.201908qweqwe00Z.f3e1e6d6d3507525.7ea9c786af32b18cedeb125ca46cc2d9ee154e09',
+    giphy = '7f6ws7Evslqweqwee9BieyYnD3OkkT',
+    token_dropbox = 's4rxVFP2mcqweqweeXinDL33K0tSIhnR1chqOmrtBPy-Dl6Ll0znnRP3Phm5',
+    token_imgur = "ebfc25qweqwe96e5",
+    token_bitly = ["a001cef9d44ed808qweqwe52d475e98079e60577", ],  #link?
 )
 """
 
